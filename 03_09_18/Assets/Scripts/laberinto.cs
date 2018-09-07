d7306a856f7e4c217b17edeb16c790c05ab2e201
dottyy <dottykurmi123@gmail.com>
2018-09-07 17:35:34 -0400

    Add files via upload

---
+++ 03_09_18/Assets/Scripts/laberinto.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class laberinto : MonoBehaviour {
+
+    // Use this for initialization
+    public float velocidad = 3;
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(Input.GetAxis("Horizontal") * velocidad * Time.deltaTime, 0, Input.GetAxis("Vertical") * velocidad * Time.deltaTime);
+		
+	}
+}
---
+++ 03_09_18/Assets/Scripts/movimientoFisico.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimientoFisico : MonoBehaviour {
+
+    // Use this for initialization
+    public float forceValue;
+    private Rigidbody rigidbody;
+    private AudioSource sonido;
+	void Start ()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        sonido = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        rigidbody.AddForce(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * 2);
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(rigidbody.velocity.y) < 0.01f)
+        {
+            rigidbody.AddForce(Vector3.up * 4, ForceMode.Impulse);
+            sonido.Play();
+        }
+	}
+}
